debf080720b6e1f30d19c9adcee3b40f0617893e
Franko Stipanov <fstipanov@gmail.com>
2021-12-06 14:07:26 +0100

    6/86t

---
+++ app.js
@@ -0,0 +1,10 @@
+let message = 'Hello ';
+
+let userName = 'Franko';
+console.log(message + userName);
+message = 'Bye ';
+
+console.log(message + userName);
+userName = 'David';
+
+console.log(message + userName);
