918c1d32eeb9f2687be34f92e16d9e21672e7775
fstipo <fstipanov@gmail.com>
2021-12-07 16:15:21 +0100

    data types

--- app.js
+++ app.js
@@ -11,4 +11,42 @@ temp = Number(true);
 temp = Number(false);
 temp = Number([1, 2, 3, 4]);
 
+// Type Coercion
+temp = 10;
+temp = temp + 5;
+temp = temp + 'five';
 console.log(temp);
+
+'Hello' + 'World';
+'5' + 5;
+5 + '5';
+'Hello' - 'World';
+'10' - 5;
+
+typeof '10';
+typeof 10;
+typeof false;
+typeof null;
+typeof undefined;
+
+temp = 5;
+typeof temp;
+a = String(temp);
+typeof a;
+let b = (100).toString();
+console.log(b);
+typeof b;
+
+const c = '150';
+const d = Number(c);
+
+typeof c;
+typeof d;
+
+console.log(typeof c, typeof d);
+
+let ab = '5';
+let cd = '15';
+
+console.log(ab + cd);
+console.log(cd - ab);
