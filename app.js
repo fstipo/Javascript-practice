bf8909add548ae36329e4cbdf7d4821c5a360ca7
fstipo <fstipanov@gmail.com>
2021-12-07 17:13:16 +0100

    ternary operatore

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
-// // Challenge #2
-// let miles = prompt('How many miles?');
-// miles = Number(miles);
-// const km = miles * 1.60934;
-// const message = `${miles} Miles is ${km} km`;
-
-// console.log(message);
+// // if else
 
 // let num = 5;
 
-// if (num) {
-//   console.log('Number exists');
-// }
-
-// num = 51;
 // if (num > 50) {
-//   console.log('Hello');
+//   console.log('Num > 50');
+// } else if (num == 50) {
+//   console.log('Num = 50');
+// } else {
+//   console.log('Num < 50');
 // }
 
-// if else
+// Ternary operator
 
-let num = 5;
+let num = prompt('Enter number');
+num = Number(num);
+console.log(num);
+let message = num % 2 ? 'odd' : 'even';
 
-if (num > 50) {
-  console.log('Num > 50');
-} else if (num == 50) {
-  console.log('Num = 50');
-} else {
-  console.log('Num < 50');
-}
+console.log(`${num} is ${message}`);
