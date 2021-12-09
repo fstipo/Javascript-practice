a1cd0ebd69dd65a2758ba420e9b0d0292992cec9
fstipo <fstipanov@gmail.com>
2021-12-09 16:06:17 +0100

    onClicks

--- app.js
+++ app.js
@@ -1,51 +1,34 @@
-// Default values
+// // Return
 
-const defaultValues = (num1 = 1, num2 = 10) => {
-  console.log(num1, num2);
-};
-defaultValues();
-defaultValues(5);
-defaultValues(undefined, undefined);
-defaultValues(20, 40);
+// const multiplier = (a, b) => {
+//   return a * b;
+// };
 
-console.log('---------------');
+// console.log(multiplier(100, 45));
+// console.log(multiplier(0, 45));
+// console.log(multiplier(2, 4));
+// console.log(multiplier(10, 5));
 
-const defaultValues2 = (num1, num2) => {
-  num1 = num1 || 15;
-  num2 = num2 || 50;
-  console.log(num1, num2);
-};
+// console.log(add2(4));
+// function add2(num) {
+//   return num + 2;
+// }
 
-defaultValues2();
-defaultValues2(25, 10);
-defaultValues2(undefined, 10);
-defaultValues2(24, undefined);
+let count1 = 0;
+let count2 = 0;
+let count3 = 0;
 
-console.log('---------------');
-
-const defaultValues3 = (num1, num2) => {
-  num1 = typeof num1 !== 'undefined' ? num1 : 580;
-  num2 = typeof num2 !== 'undefined' ? num2 : 880;
-  console.log(num1, num2);
+const message1 = () => {
+  count1++;
+  console.log(count1, count2, count3);
 };
 
-defaultValues3();
-defaultValues3(125, 100);
-defaultValues3(undefined, 105);
-defaultValues3(240, undefined);
-
-// Return
-
-const multiplier = (a, b) => {
-  return a * b;
+const message2 = () => {
+  count2++;
+  console.log(count1, count2, count3);
 };
 
-console.log(multiplier(100, 45));
-console.log(multiplier(0, 45));
-console.log(multiplier(2, 4));
-console.log(multiplier(10, 5));
-
-console.log(add2(4));
-function add2(num) {
-  return num + 2;
-}
+const message3 = () => {
+  count3++;
+  console.log(count1, count2, count3);
+};
