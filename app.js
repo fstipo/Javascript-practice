8a8d9edebbb0c1174463e056d4c21def2861ff79
fstipo <fstipanov@gmail.com>
2021-12-09 17:05:58 +0100

    Function declaration vs. function expression

--- app.js
+++ app.js
@@ -14,31 +14,16 @@
 //   return num + 2;
 // }
 
-const btns = document.querySelectorAll('button');
+add2(20);
 
-btns[0].onclick = message1;
-btns[1].onclick = message2;
-btns[2].onclick = message3;
-
-let count1 = 0;
-let count2 = 0;
-let count3 = 0;
-
-const message = () => {
-  console.log(count1, count2, count3);
+// Function Expression
+const add1 = (num) => {
+  console.log(num * 5);
 };
 
-function message1() {
-  count1++;
-  message();
+// Function declaration
+function add2(num) {
+  console.log(num * 5);
 }
 
-function message2() {
-  count2++;
-  message();
-}
-
-function message3() {
-  count3++;
-  message();
-}
+add1(2);
