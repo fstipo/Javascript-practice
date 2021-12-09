d76b312ab68faf95caae387ea4962726306a5f42
fstipo <fstipanov@gmail.com>
2021-12-09 17:59:26 +0100

    Function recursion

--- app.js
+++ app.js
@@ -1,41 +1,65 @@
-// // Return
-
-// const multiplier = (a, b) => {
-//   return a * b;
+// // Function Expression
+// const add1 = (num) => {
+//   console.log(num * 5);
 // };
 
-// console.log(multiplier(100, 45));
-// console.log(multiplier(0, 45));
-// console.log(multiplier(2, 4));
-// console.log(multiplier(10, 5));
-
-// console.log(add2(4));
+// // Function declaration
 // function add2(num) {
-//   return num + 2;
+//   console.log(num * 5);
 // }
 
-add2(20);
+// add1(2);
 
-// Function Expression
-const add1 = (num) => {
-  console.log(num * 5);
+// // Function scope
+// let a = 'Hello';
+// let b;
+
+// const test1 = () => {
+//   b = 'World';
+//   console.log(`${a} ${b}`);
+// };
+
+// test1();
+// console.log(b);
+
+// Function recursion
+
+const loop = (x) => {
+  if (x >= 10) return;
+  console.log(x);
+  loop(x + 2);
+};
+loop(0);
+loop(5);
+
+let secretNumber = 8;
+let guess;
+let counter = 0;
+
+const guesser = () => {
+  let guess = Number(
+    prompt(`Guess the number. Enter number 0 - 50
+  `)
+  );
+  console.log(guess, secretNumber);
+  counter++;
+
+  if (guess === secretNumber) {
+    console.log(`${guess} is correct number.`);
+    console.log(`You are winner 🏆`);
+    console.log('Counter:' + counter);
+    return;
+  } else if (guess < secretNumber) {
+    console.log('Your number is smaller!', 'Counter: ' + counter);
+    guesser();
+  } else if (guess > secretNumber) {
+    console.log('Your number is bigger!', 'Counter: ' + counter);
+    guesser();
+  } else {
+    counter++;
+    console.log('WRONG!', 'Counter: ' + counter);
+    guesser();
+  }
 };
 
-// Function declaration
-function add2(num) {
-  console.log(num * 5);
-}
-
-add1(2);
-
-// Function scope
-let a = 'Hello';
-let b;
-
-const test1 = () => {
-  b = 'World';
-  console.log(`${a} ${b}`);
-};
-
-test1();
-console.log(b);
+guesser();
