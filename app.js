e13f90d0596ba18ca5ca794cf2d59de90689591a
fstipo <fstipanov@gmail.com>
2021-12-10 08:24:48 +0100

    IIFE

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-// // Function Expression
-// const add1 = (num) => {
-//   console.log(num * 5);
-// };
-
-// // Function declaration
-// function add2(num) {
-//   console.log(num * 5);
-// }
-
-// add1(2);
-
-// // Function scope
-// let a = 'Hello';
-// let b;
-
-// const test1 = () => {
-//   b = 'World';
-//   console.log(`${a} ${b}`);
-// };
-
-// test1();
-// console.log(b);
-
 /* Function recursion*/
 
 // const loop = (x) => {
@@ -32,21 +8,39 @@
 
 // loop(0);
 
-let secretNumber = 11;
-let guess;
+// let secretNumber = 11;
+// let guess;
 
-const guesser = () => {
-  guess = Number(prompt('Guess the number'));
-  console.log(guess);
-  if (guess === secretNumber) {
-    console.log('Number is correct');
-    console.log('You win!!! 🏆');
-    return;
-  } else {
-    let message = guess < secretNumber ? 'higher' : 'lower';
-    console.log(`Wrong GO ${message}!`);
-    guesser();
-  }
-};
+// const guesser = () => {
+//   guess = Number(prompt('Guess the number'));
+//   console.log(guess);
+//   if (guess === secretNumber) {
+//     console.log('Number is correct');
+//     console.log('You win!!! 🏆');
+//     return;
+//   } else {
+//     let message = guess < secretNumber ? 'higher' : 'lower';
+//     console.log(`Wrong GO ${message}!`);
+//     guesser();
+//   }
+// };
 
-guesser();
+// guesser();
+
+// IIFE
+
+(function () {
+  console.log('Welcome');
+  console.log('--------------');
+})();
+
+(function (firstName) {
+  console.log(`My name is ${firstName}`);
+  console.log('--------------');
+})('Franko');
+
+let result = (() => {
+  return 'Hello World!';
+})();
+
+console.log(result);
