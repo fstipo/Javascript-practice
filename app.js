fd5bb94fcc36e66be40ae802671a81d2d7488a58
fstipo <fstipanov@gmail.com>
2021-12-11 10:30:31 +0100

    Include,indexOf,Slice,Splice,reverse,join,concat

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const arr = ['a', 'b', 'c', 'd', 'e'];
+const arr1 = [1, 2, 3, 45, 55];
 arr;
 arr.push('tom');
 arr.unshift('Betty');
@@ -10,5 +11,16 @@ arr.indexOf('e');
 arr.length;
 arr.includes('5');
 arr.includes('d');
+// arr.slice(-4);
+// arr.splice(1, 3);
+arr.reverse();
+arr.concat(arr1).reverse();
+[...arr, ...arr1];
 
 console.log(arr);
+let arrSum = arr.join('');
+arrSum = arr.join('-');
+arrSum = arr.join('8');
+arrSum = arr.join('/*');
+
+console.log(arrSum);
