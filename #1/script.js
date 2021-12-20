087f32f2fdae656e5ba1f678f530d3dd575255b7
fstipo <fstipanov@gmail.com>
2021-12-20 16:01:52 +0100

    DOM selection

--- #1/script.js
+++ #1/script.js
@@ -6,3 +6,14 @@ console.dir(document);
 document.querySelector('h1').innerText = 'Is this working?';
 console.log(document.URL);
 console.log(document.location);
+
+document.getElementById('myID').style.backgroundColor = 'blue';
+document.querySelector('p').style.backgroundColor = 'yellow';
+document.querySelector('p').style.backgroundColor = 'yellow';
+document.querySelector('.first').style.color = 'red';
+document.querySelector('.first span').style.color = 'purple';
+document.querySelector('span:nth-child(5)').style.color = 'purple';
+document.querySelector('li:last-child').style.display = 'none';
+document.querySelector('li:last-child').style.backgroundColor = 'blue';
+document.querySelector('li:nth-child(3)').style.backgroundColor = 'green';
+document.querySelector('li:nth-child(5)').style.backgroundColor = 'purple';
