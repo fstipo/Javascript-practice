556a8647041d292affcc776c2e42356bd5503118
fstipo <fstipanov@gmail.com>
2021-12-21 14:48:53 +0100

    DOM manipulation practice

--- #1/script.js
+++ #1/script.js
@@ -1,26 +1,28 @@
 // 'use strict';
 
-// Element manipulation
+// select element , all - foreach , for
+// change bgColor , add id, add content
+// remove element, get attribute, set attribute href
 
-// remove div with class pickMe
-const divPickMeEl = document.querySelector('.pickMe');
-divPickMeEl.style['background-color'] = 'yellow';
-divPickMeEl.remove();
+const h1El = document.getElementById('myID');
+console.log(h1El);
 
-// Update list items IDs with count
+const spanEl = document.getElementsByTagName('span');
+console.log(spanEl);
 
-// const liListEl = document.querySelectorAll('li');
-// console.log(liListEl);
-// liListEl.forEach((el, i) => {
-//   el.id = `li${i + 1}`;
-//   el.textContent = 'list item #' + i;
-//   if (el.getAttribute('class')) {
-//     console.log(el.getAttribute('class'));
-//   }
-// });
+for (let i = 0; i < spanEl.length; i++) {
+  console.log(spanEl[i]);
+  spanEl[i].style.backgroundColor = 'green';
+}
 
-const liList = document.querySelectorAll('a');
-liList.forEach((el) => {
-  el.setAttribute('href', 'https://www.muzika.hr/');
+const listEl = document.querySelectorAll('a');
+console.log(listEl);
+
+listEl.forEach((el, i) => {
   console.log(el);
+  el.id = 'list' + (i + 1);
+  el.textContent = 'list item #' + i;
+  if (el.getAttribute('class')) console.log(el.getAttribute('class'), i);
+  el.setAttribute('href', 'https://www.torrenthr.org/browse2.php');
 });
+h1El.remove();
