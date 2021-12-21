932e2282fa0a016d618f6948bd9d59e715117f73
fstipo <fstipanov@gmail.com>
2021-12-21 15:20:35 +0100

    Element classes #1

--- #1/script.js
+++ #1/script.js
@@ -1,28 +1,16 @@
 // 'use strict';
 
-// select element , all - foreach , for
-// change bgColor , add id, add content
-// remove element, get attribute, set attribute href
+// Element classes
 
-const h1El = document.getElementById('myID');
-console.log(h1El);
+const listItem = document.querySelectorAll('li');
+console.log(listItem);
 
-const spanEl = document.getElementsByTagName('span');
-console.log(spanEl);
-
-for (let i = 0; i < spanEl.length; i++) {
-  console.log(spanEl[i]);
-  spanEl[i].style.backgroundColor = 'green';
-}
-
-const listEl = document.querySelectorAll('a');
-console.log(listEl);
-
-listEl.forEach((el, i) => {
-  console.log(el);
-  el.id = 'list' + (i + 1);
-  el.textContent = 'list item #' + i;
-  if (el.getAttribute('class')) console.log(el.getAttribute('class'), i);
-  el.setAttribute('href', 'https://www.torrenthr.org/browse2.php');
+listItem.forEach((el) => {
+  el.classList && (el.innerText = el.classList);
+  el.classList.contains('first') || (el.innerText = 'no Class');
+  el.classList.add('test2');
+  el.classList.remove('test2');
+  el.classList.toggle('test3');
+  el.classList.add('test1');
+  el.classList.add('test4');
 });
-h1El.remove();
