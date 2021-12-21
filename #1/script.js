73b5cdaa775947217d8c59ff48c1d0647cb5e4b7
fstipo <fstipanov@gmail.com>
2021-12-21 15:26:08 +0100

    Element classes #2

--- #1/script.js
+++ #1/script.js
@@ -6,11 +6,10 @@ const listItem = document.querySelectorAll('li');
 console.log(listItem);
 
 listItem.forEach((el) => {
-  el.classList && (el.innerText = el.classList);
-  el.classList.contains('first') || (el.innerText = 'no Class');
-  el.classList.add('test2');
-  el.classList.remove('test2');
-  el.classList.toggle('test3');
-  el.classList.add('test1');
-  el.classList.add('test4');
+  console.log(el);
+  el.innerText = el.className ? el.className : 'no Class';
+  el.classList.add('test');
+  el.classList.toggle('first');
+  el.classList.remove('test');
+  el.classList.replace('first', 'test5');
 });
