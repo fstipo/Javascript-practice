7105561ffbd5b4bc5f450f663f660e02ec6d74a2
fstipo <fstipanov@gmail.com>
2021-12-22 09:26:38 +0100

    Html Collection vs. Node list

--- #1/script.js
+++ #1/script.js
@@ -2,18 +2,36 @@
 
 // Element classes
 
-const listItems = document.querySelectorAll('li');
-// console.log(listItems);
+const el = document.querySelector('div.first');
+console.dir(el);
 
-for (let i = 0; i < listItems.length; i++) {
-  // console.log(listItems[i].className);
-  listItems[i].textContent = listItems[i].className
-    ? listItems[i].className
-    : 'No Class';
-  const el = listItems[i];
-  el.classList.add('test');
-  el.classList.toggle('first');
-  el.classList.remove('test');
-  el.classList.replace('first', 'abrakadabra');
-  console.log(el);
+console.log(el.children);
+// length
+console.log(el.children.length);
+console.log(el.childNodes.length);
+
+// for loop
+for (let i = 0; i < el.children.length; i++) {
+  console.log(el.children[i].textContent);
 }
+
+for (let i = 0; i < el.childNodes.length; i++) {
+  console.log(el.children[i]);
+}
+
+// Doesn't work with HTMLCollection
+// // forEach
+// el.children.forEach((el) => {
+//   console.log(el);
+// });
+
+el.childNodes.forEach((el) => {
+  console.log(el);
+});
+
+console.log(el.parentElement);
+console.log(el.parentNode);
+console.log(el.nextElementSibling);
+console.log(el.nextSibling);
+console.log(el.previousElementSibling);
+console.log(el.previousSibling);
