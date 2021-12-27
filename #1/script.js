d4a631d104b0160b98180c8fe5353d5e9beeca5e
fstipo <fstipanov@gmail.com>
2021-12-27 13:02:09 +0100

    change color on span

--- #1/script.js
+++ #1/script.js
@@ -11,7 +11,25 @@ const changeColor = () => {
     255
   )})`;
   document.body.style.backgroundColor = temp;
-  console.log(temp);
 };
 
 btn.addEventListener('click', changeColor);
+
+const spanEl = document.querySelectorAll('span');
+console.log(spanEl);
+
+spanEl.forEach((el) => {
+  console.log(el);
+  el.style.border = '1px solid black';
+  el.style.width = '80vW';
+  el.style.padding = '20px';
+  el.addEventListener('click', () => {
+    let temp = `rgb(${randomNumber(255)},${randomNumber(255)},${randomNumber(
+      255
+    )})`;
+    el.style.backgroundColor = temp;
+    el.style.color = 'white';
+    el.style['font-weight'] = 'bold';
+    el.innerText = temp;
+  });
+});
