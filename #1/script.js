d62d114c35414aa343a520afad2cc67cc8af03c2
fstipo <fstipanov@gmail.com>
2021-12-27 12:49:12 +0100

    change color

--- #1/script.js
+++ #1/script.js
@@ -1,20 +1,17 @@
 // 'use strict';
 
 // add item
-const inputEl = document.querySelector('input');
 const btn = document.querySelector('button');
-const itemList = document.querySelector('.second');
-console.log(inputEl);
-console.log(btn);
-console.log(itemList);
-
-const addItem = () => {
-  console.log(inputEl.value);
-  const li = document.createElement('li');
-  li.innerText = inputEl.value;
-  if (inputEl.value.length > 3) {
-    itemList.appendChild(li);
-  }
+const randomNumber = (num) => {
+  return Math.floor(Math.random() * (num + 1));
 };
 
-btn.addEventListener('click', addItem);
+const changeColor = () => {
+  let temp = `rgb(${randomNumber(255)},${randomNumber(255)},${randomNumber(
+    255
+  )})`;
+  document.body.style.backgroundColor = temp;
+  console.log(temp);
+};
+
+btn.addEventListener('click', changeColor);
