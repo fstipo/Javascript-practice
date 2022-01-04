1842904413d2be8e220900ed45196da28c25d0e2
fstipo <fstipanov@gmail.com>
2022-01-04 16:52:08 +0100

    string methods

--- #1/script.js
+++ #1/script.js
@@ -13,3 +13,30 @@ console.log(typeof num1.toFixed(1));
 console.log(parseInt(num2));
 console.log(Number(true));
 console.log(Number('true'));
+console.log('------------------------------------------');
+
+// String Methods
+
+const myString =
+  'Hello World, JavaScript is the best programming language in the world';
+
+// console.log(myString.length);
+// console.log(myString);
+// console.log(myString.trim());
+// console.log(myString.toLowerCase().trim());
+// console.log(myString.toUpperCase().trim());
+// console.log(myString.trim().split(' '));
+// console.log(myString.charAt(4));
+// console.log(myString.charAt(14));
+// console.log(myString[7]);
+console.log(myString.slice(2, 5));
+console.log(myString.slice(13, 23));
+console.log(myString.substring(13, 23));
+console.log(myString.substr(13, 23));
+console.log(myString.startsWith('Hello'));
+
+console.log(myString.replace('best', 'bebebe'));
+console.log(myString.indexOf('world'));
+console.log(myString.indexOf('World'));
+console.log(myString.search('the'));
+console.log(myString.search('thee'));
