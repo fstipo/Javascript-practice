29415ace106eebea3705db5bf4b3bcfa50aa4de4
fstipo <fstipanov@gmail.com>
2022-01-04 15:47:12 +0100

    capturing, bubble

--- #1/script.js
+++ #1/script.js
@@ -3,30 +3,32 @@
 // Event Bubbling and Capturing
 
 const divElements = document.querySelectorAll('div');
-console.log(divElements);
 const outputEl = document.querySelector('section');
-console.log(outputEl);
 
-const output = (msg) => {
-  outputEl.innerHTML += `${msg} <br>`;
-};
+const random = (num) => Math.floor(Math.random() * num);
 
-divElements.forEach((el, index) => {
-  el.style.border = '1px solid red';
-  el.style.width = '100px';
+const randomColor = `rgb(${random(256)},${random(256)},${random(256)})`;
+const msg = divElements.forEach((el, index) => {
+  el.style.border = '1px solid green';
   el.style.padding = '20px';
+  el.style.width = '120px';
+  el.style.width = '120px';
+  el.style.margin = '30px';
   el.v = index + 1;
   el.addEventListener(
     'click',
-    () => {
-      output('capture: ' + el.v);
+    function () {
+      el.style.backgroundColor = randomColor;
+      outputEl.innerHTML += `   capturing:  +${this.v} <br>
+      `;
     },
     true
   );
   el.addEventListener(
     'click',
-    () => {
-      output('bubble:' + el.v);
+    function () {
+      this.style.backgroundColor = randomColor;
+      outputEl.innerHTML += `   bubble:  -${this.v} <br>`;
     },
     false
   );
