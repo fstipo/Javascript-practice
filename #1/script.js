be6d867fd28d5aa3e7790fec43ed1f492857ce31
fstipo <fstipanov@gmail.com>
2022-01-05 18:14:49 +0100

    Random message

--- #1/script.js
+++ #1/script.js
@@ -1,56 +1,19 @@
 'use strict';
 
-// // Includes + ternary
-// // const arr = [1, 2, 5, 8];
-
-// // const isNumber = (val) => {
-// //   let temp = val ? 'yes' : 'No';
-// //   console.log(temp);
-// // };
-// // isNumber(arr.includes(5));
-// // isNumber(arr.includes(8));
-// // isNumber(arr.includes(81));
-// // isNumber(arr.includes(1));
-
-// const excludeNumbers = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
-// const randomNumber = (min, max) => {
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-//   let num = Math.floor(Math.random() * (max - min) + min);
-//   return excludeNumbers.includes(num) ? randomNumber(min, max) : num;
-// };
-
-// for (let i = 0; i < 50; i++) {
-//   let num = randomNumber(1, 20);
-//   console.log(num);
-// }
-
-// Math Methods
-// const arr = [1, 3, 5, 7, 9];
-
-// console.log(arr.includes(1));
-// console.log(arr.includes(10));
-
-// const isIncluded = (num) => {
-//   let temp = num ? 'yes' : 'No';
-//   console.log(temp);
-// };
-
-// isIncluded(arr.includes(1));
-// isIncluded(arr.includes(2));
-// isIncluded(arr.includes(3));
-// isIncluded(arr.includes(4));
-// isIncluded(arr.includes(14));
-
-const arrExclude = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
-const randomNumbers = (min, max) => {
-  min = Math.floor(min);
-  max = Math.floor(max);
-  let num = Math.floor(Math.random() * (max - min) + min);
-  return arrExclude.includes(num) ? randomNumbers(min, max) : num;
+const message = ['Hello', 'Hi', 'Bye Bye', 'Bla bla', 'Aloha', 'Good Night'];
+const divEl = document.querySelector('div');
+const randomText = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
 };
+divEl.style.backgroundColor = 'yellow';
+divEl.style.textAlign = 'center';
+divEl.style.fontSize = '56px';
+divEl.style.width = '50%';
+divEl.innerText = randomText(message);
+divEl.addEventListener('click', () => {
+  divEl.innerText = randomText(message);
+});
 
-for (let i = 0; i < 20; i++) {
-  let num = randomNumbers(1, 21);
-  console.log(num);
+for (let i = 0; i < 10; i++) {
+  console.log(randomText(message));
 }
