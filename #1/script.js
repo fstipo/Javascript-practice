778e8e3be27f972195181985866e7429c47ce044
fstipo <fstipanov@gmail.com>
2022-01-06 08:04:20 +0100

    random hex color

--- #1/script.js
+++ #1/script.js
@@ -1,27 +1,9 @@
 'use strict';
 
-const msg = ['Hello', 'Bye', 'Hi', 'Good morning', 'Hello beautiful'];
-
-const randomMsg = (arr) => {
-  let temp = arr[Math.floor(Math.random() * arr.length)];
-  return temp;
+const changeColor = (el) => {
+  let hexNum = Math.random().toString(16).substr(-6);
+  document.body.style.backgroundColor = `#${hexNum}`;
 };
 
-const randomColor = (num) => {
-  const randomNumber = (num) => Math.floor(Math.random() * num);
-  let temp = `rgb(${randomNumber(num)},${randomNumber(num)},${randomNumber(
-    num
-  )})`;
-  return temp;
-};
-
-for (let i = 0; i < 10; i++) {
-  const divEl = document.createElement('div');
-  divEl.style.fontSize = '24px';
-  divEl.style.padding = '14px';
-  divEl.style.backgroundColor = randomColor(255);
-  divEl.innerHTML = randomMsg(msg);
-  document.body.appendChild(divEl);
-}
-
-randomColor(2);
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', changeColor);
