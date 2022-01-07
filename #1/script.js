722cb0ac8c7f39caba10ffa261dc161c6ad1e04d
fstipo <fstipanov@gmail.com>
2022-01-07 15:23:00 +0100

    Random msg + Random color

--- #1/script.js
+++ #1/script.js
@@ -14,15 +14,15 @@ const randomGreeting = (greetings) => {
   return greetings[Math.floor(Math.random() * greetings.length)];
 };
 
-const randomNum = (num) => {
-  return Math.floor(Math.random() * num);
-};
+const randomNum = (num) => Math.floor(Math.random() * num);
 
 for (let i = 0; i < 10; i++) {
   let divEl = document.createElement('div');
   divEl.innerText = randomGreeting(msg);
   divEl.style.fontSize = '24px';
   divEl.style.padding = '10px';
-  divEl.style.backgroundColor = 'lime';
+  divEl.style.backgroundColor = `rgb(${randomNum(256)},${randomNum(
+    256
+  )},${randomNum(256)})`;
   document.body.appendChild(divEl);
 }
