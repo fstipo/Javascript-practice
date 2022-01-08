fa9c44066fa87d0a68f0f1da8e58983bd596e9a2
fstipo <fstipanov@gmail.com>
2022-01-08 07:25:34 +0100

    document loaded vs. window loaded

--- #1/script.js
+++ #1/script.js
@@ -1,44 +1,12 @@
 'use strict';
 
-const player = {
-  speed: 50,
-  x: 400,
-  y: 300,
-};
+// Document load and than window load
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Document is loaded');
+  console.log('Document is loaded');
+  console.log('Document is loaded');
+});
 
-const build = () => {
-  player.el = document.createElement('button');
-  player.el.innerText = player.y;
-  player.el.classList.add('btn');
-  player.el.style.position = 'absolute';
-  document.body.appendChild(player.el);
-};
-window.addEventListener('DOMContentLoaded', build);
-document.addEventListener('keydown', (e) => {
-  console.log(e.key);
-  if (e.key === 'ArrowUp') {
-    player.y -= player.speed;
-    player.el.innerText = player.y;
-    player.el.style.backgroundColor = 'gray';
-  }
-  if (e.key === 'ArrowDown') {
-    player.y += player.speed;
-    player.el.innerText = player.y;
-    player.el.style.backgroundColor = 'purple';
-  }
-  if (e.key === 'ArrowLeft') {
-    player.x -= player.speed;
-    player.el.innerText = player.x;
-    player.el.style.backgroundColor = 'red';
-  }
-  if (e.key === 'ArrowRight') {
-    player.x += player.speed;
-    player.el.innerText = player.x;
-    player.el.style.backgroundColor = 'green';
-  }
-
-  player.el.style.top = player.y + 'px';
-  player.el.style.left = player.x + 'px';
-
-  console.dir(player.el);
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('Window is loaded');
 });
