d09cbba4b2daefc37a85a50d3f05825c64c4efe9
fstipo <fstipanov@gmail.com>
2022-01-08 12:30:52 +0100

    my birthDay

--- #1/script.js
+++ #1/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const now = new Date();
+const now = Date.now();
 
 // const month = date.getMonth();
 // const day = date.getDay();
@@ -13,4 +13,10 @@ const birthDay = new Date(1977, 11, 30);
 console.log(birthDay.getTime());
 
 const sumYears = now - birthDay;
-console.log(sumYears / 1000 / 60 / 60 / 24 / 365);
+
+const years = parseInt(sumYears / 1000 / 60 / 60 / 24 / 365);
+const days = parseInt(sumYears / 1000 / 60 / 60 / 24);
+const hours = parseInt(sumYears / 1000 / 60 / 60);
+const minutes = parseInt(sumYears / 1000 / 60);
+const seconds = parseInt(sumYears / 1000);
+const milSeconds = parseInt(sumYears);
