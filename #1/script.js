5210325106600295e67eaecce38ef98bde2729b8
fstipo <fstipanov@gmail.com>
2022-01-10 06:36:00 +0100

    Practice Random

--- #1/script.js
+++ #1/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const numbers = [14, 2, 19, 6, 8, 13, 11, 12];
+const numbers = [14, 2, 19, 6, 8, 13, 11, 12, 16, 17, 18, 19, 20];
 
 console.log(numbers.includes(2));
 console.log(numbers.includes(12));
@@ -16,3 +16,14 @@ isIncluded(numbers.includes(12));
 isIncluded(numbers.includes(14));
 isIncluded(numbers.includes(10));
 isIncluded(numbers.includes(3));
+
+const randomNumber = (min, max) => {
+  min = Math.floor(min);
+  max = Math.ceil(max);
+  let num = Math.floor(Math.random() * (max - min) + min);
+  return numbers.includes(num) ? randomNumber(min, max) : num;
+};
+
+for (let i = 0; i <= 10; i++) {
+  console.log(randomNumber(1, 21));
+}
