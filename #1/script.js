2c4e03414dda989c37e128a4a4e5fbd28c723162
fstipo <fstipanov@gmail.com>
2022-01-10 18:23:34 +0100

    Locale storage

--- #1/script.js
+++ #1/script.js
@@ -1,27 +1,31 @@
 'use strict';
-const d = new Date();
-console.log(d.getTime());
-console.log(d);
-console.log(d.getDate());
-console.log(d.getMonth());
-console.log(d.getFullYear());
-console.log(d.getHours());
-console.log(d.getMinutes());
-console.log(d.getSeconds());
 
-const today = Date.now();
-console.log(today);
+// Local Storage
 
-const myBirthday = new Date('1977-11-30');
-console.log(myBirthday);
+if (localStorage.getItem('num')) {
+  let cnt = localStorage.getItem('num');
+  cnt = Number(cnt);
+  cnt++;
+  console.log(cnt);
+  localStorage.setItem('num', cnt);
+} else {
+  localStorage.setItem('num', '1');
+}
 
-const sum = today - myBirthday;
-let milSeconds = sum / 1000 / 60 / 60 / 24 / 365;
+console.log(localStorage.getItem('num'));
 
-console.log(milSeconds);
-let yearsOld = parseInt(sum / 1000 / 60 / 60 / 24 / 365);
-console.log('Years:' + yearsOld);
-const months = (milSeconds - 44) * 12;
-console.log('months:' + Math.floor(months));
-const days = (months - 1) * 24;
-console.log('days:' + parseInt(days));
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+  localStorage.clear();
+});
+
+const myObject = { name: 'Franko', lastName: 'Stipanov' };
+const myString = JSON.stringify(myObject);
+
+localStorage.setItem('my', myString);
+
+const newString = localStorage.getItem('my');
+const newObject = JSON.parse(newString);
+
+console.log(newString);
+console.log(newObject.lastName);
