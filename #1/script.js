79a2de2ba8e343437a74a50acb253530464a3d79
fstipo <fstipanov@gmail.com>
2022-01-10 06:48:20 +0100

    Practice random msg, random color,

--- #1/script.js
+++ #1/script.js
@@ -1,29 +1,25 @@
 'use strict';
 
-const numbers = [14, 2, 19, 6, 8, 13, 11, 12, 16, 17, 18, 19, 20];
+const greetings = ['Hello', 'Bye Bye', 'Stop', 'Good Evening', '🚀🔴', '🎃😬'];
 
-console.log(numbers.includes(2));
-console.log(numbers.includes(12));
-console.log(numbers.includes(14));
-console.log(numbers.includes(10));
-
-const isIncluded = (num) => {
-  console.log(num ? 'yes' : 'No');
+const randomGreeting = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
 };
 
-isIncluded(numbers.includes(2));
-isIncluded(numbers.includes(12));
-isIncluded(numbers.includes(14));
-isIncluded(numbers.includes(10));
-isIncluded(numbers.includes(3));
-
-const randomNumber = (min, max) => {
-  min = Math.floor(min);
-  max = Math.ceil(max);
-  let num = Math.floor(Math.random() * (max - min) + min);
-  return numbers.includes(num) ? randomNumber(min, max) : num;
-};
+const randomNumber = (num) => Math.floor(Math.random() * num);
 
 for (let i = 0; i <= 10; i++) {
-  console.log(randomNumber(1, 21));
+  let divEl = document.createElement('div');
+  divEl.innerText = randomGreeting(greetings);
+  divEl.style.fontSize = '24px';
+  divEl.style.padding = '14px';
+  divEl.style.backgroundColor = `rgb(${randomNumber(256)},${randomNumber(
+    256
+  )},${randomNumber(256)})`;
+  document.body.appendChild(divEl);
+  console.log(
+    `rgb(${randomNumber(256)},rgb(${randomNumber(256)},rgb(${randomNumber(
+      256
+    )})`
+  );
 }
