ebe17df74b5c979cb4272982fced75521022a4ba
fstipo <fstipanov@gmail.com>
2022-01-13 19:12:44 +0100

    band list

--- #1/script.js
+++ #1/script.js
@@ -2,32 +2,27 @@
 
 // getBoundingClientRect
 
+// const BandList =
+
+const inputEl = document.querySelector('input');
 const btn = document.querySelector('.btn');
-btn.getElementsByClassName.position = 'absolute';
-const elPosition = btn.getBoundingClientRect();
-let speed = 1;
 
-const move = (e) => {
-  if (e.key === 'ArrowUp') {
-    elPosition.y -= speed;
-  }
-  if (e.key === 'ArrowDown') {
-    elPosition.y += speed;
-  }
-  if (e.key === 'ArrowLeft') {
-    elPosition.x -= speed;
-  }
-  if (e.key === 'ArrowRight') {
-    elPosition.x += speed;
-  }
-  btn.innerHTML = `x:${elPosition.x} <br> y:${elPosition.y}`;
-  btn.style.left = elPosition.x + 'px';
-  btn.style.top = elPosition.y + 'px';
-};
-
-// Object properties
-for (let index in elPosition) {
-  console.log(index, elPosition[index]);
+if (localStorage.getItem('bandList')) {
+  let temp = localStorage.getItem('bandList');
+  console.log(temp);
+} else {
+  localStorage.setItem('bandList', []);
 }
 
-document.addEventListener('keydown', move);
+const bandList = [];
+
+const addItem = () => {
+  const item = inputEl.value;
+  bandList.push(item);
+  let temp = JSON.stringify(bandList);
+  inputEl.value = '';
+  localStorage.setItem('bandList', temp);
+  console.log(bandList);
+};
+
+btn.addEventListener('click', addItem);
